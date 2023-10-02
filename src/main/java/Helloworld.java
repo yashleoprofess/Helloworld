892a394b55654971f3540b4e33bdb92fa5493347
hemasri <bhemasri56@gmail.com>
2023-10-02 17:49:21 +0530

    Added welcome meassage twice

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -1,6 +1,7 @@
 public class Helloworld {
     public static void main(String[] args){
         System.out.println("Hello Welcome to java Mr.yaswanth");
+        System.out.println("once again Welcome to java world");
     }
 }
 
